04fdc05ee809cbb617b7f8381a82429fbb9ee174
vedhasri saluru <saluruvedhasri@gmail.com>
2025-11-24 15:25:44 +0530

    Job description

--- src/components/employer/SecuroPublish.vue
+++ src/components/employer/SecuroPublish.vue
@@ -23,19 +23,18 @@
                             <div class="div-table" id="borderLeft">
                                 <div class="div-table" style="background-color: seashell;">
                                     <div class="div-table-row" style="background-color: #ccc;">
-                                        <div class="div-table-col3"><b>Opportunity.Code</b></div>
+                                        <div class="div-table-col1"><b>Oppurtunity Code</b></div>
                                         <div class="div-table-col6"><b>Designation</b></div>
-                                        <div class="div-table-col3A"><b>Publish.Dt</b></div>
-
+                                        <div class="div-table-col1"><b>Last Date</b></div>
                                     </div>
 
                                     <div class="div-table-row" v-for="d in postdata" :key="d.id">
-                                        <div class="div-table-col3">
-                                            <a @click="viewJd(d)"><span style="color: blueviolet">{{ d.oppurtunity_code }}</span></a>
+                                        <div class="div-table-col1">
+                                            <a @click="viewJd(d)"><span style="color: blueviolet">{{ d.oppurtunityCode }}</span></a>
                                         </div>
 
-                                        <div class="div-table-col6">{{ d.oppurtunity_designation }}</div>
-                                         <div class="div-table-col3A">{{ d.oppurtunity_postDate }}</div>
+                                        <div class="div-table-col6">{{ d.oppurtunityDesignation}}</div>
+                                         <div class="div-table-col1">{{ toUI(d.oppurtunityLastDate)}}</div>
 
                                     </div>
                                 </div>
@@ -47,7 +46,7 @@
          
              
                     <div v-if="visible">
-                    <h4 style="color: slateblue;">Opportunity.Code - {{ jdcode }}</h4>
+                    <h4 style="color: slateblue;">Opportunity Code - {{ jdcode }}</h4>
                    
                 <div class="div-table" >
                         <div class="col-md-12 col-sm-12">
@@ -69,7 +68,7 @@
                                 <b-label class="flright item">:</b-label>
                             </div>
                             <div class="col-md-7 col-sm-7">
-                                {{ postDate }}
+                                {{toUI(postDate) }}
                             </div>
                         </div>
                         <div class="col-md-12 col-sm-12">
@@ -80,7 +79,7 @@
                                 <b-label class="flright item">:</b-label>
                             </div>
                             <div class="col-md-7 col-sm-7">
-                                {{ lastDate }}
+                                {{ toUI(lastDate) }}
                             </div>
                         </div>
                         <div class="col-md-12 col-sm-12">
@@ -102,7 +101,7 @@
                                 <b-label class="flright item">:</b-label>
                             </div>
                             <div class="col-md-7 col-sm-7">
-                                {{ emailId }}
+                                {{ email }}
                             </div>
                         </div>
                         <div class="col-md-12 col-sm-12">
@@ -113,7 +112,7 @@
                                 <b-label class="flright item">:</b-label>
                             </div>
                             <div class="col-md-7 col-sm-7">
-                                {{ mobile }}
+                                {{ mob }}
                             </div>
                        
                         </div>
@@ -129,7 +128,6 @@
                             </div>
                         </div>
                     </div>
-                        
                     </div>
             </div>
         </form>
@@ -137,109 +135,63 @@
 </template>
 
 <script>
-
+import axios from "axios";
 export default {
     data() {
         return {
             visible: false,
-            disabledFlag: true,
-            canApprove: false,
             jdcode : "",
-            empname: "",
             postDate : "",
-            lastDate : "",
-            incharge : "",
-            designation : "",
-            emailId : "",
-            mobile : "",
-            description : "",
-            location : "",
+            designation: "",
+            email: "",
+            lastDate: " ",
+            description: "",
+            incharge: "",
+            location: "",
+            mob: "",
+            postdata: [],
+        };
+    },
+methods: {
+        async fetchAll() {
+  try {
+    const res = await axios.get(`${this.$hostName}/api/v1/oppurtunity`);
+    this.postdata = res.data;
+  } catch (err) {
+    console.error("Fetch All Error:", err);
+  }
+},
+ viewJd(d) {
+  this.visible = true;
+  this.jdcode = d.oppurtunityCode;
+  this.designation = d.oppurtunityDesignation;
+  this.email = d.oppurtunityEmail;
+  this.mob = d.oppurtunityPhone;
+  this.incharge = d.oppurtunityIncharge;
+  this.description = d.oppurtunityJobDescription;
+  this.location = d.oppurtunityLocation;
+  this.lastDate = d.oppurtunityLastDate;
+  this.postDate = d.oppurtunityCreateDate;
+},
 
-            postdata: [
-                {
-                    oppurtunity_id: 1,
-                    oppurtunity_code: 'AF16555',
-                    employer_name: 'AFC INDIA LIMITED',
-                    oppurtunity_designation: 'Jr.Engineer',
-                    oppurtunity_postDate: '29-01-2025',
-                    oppurtunity_lastDate: '02-02-2025',
-                    oppurtunity_incharge: 'Ms.Stella',
-                    oppurtunity_email: 'hr@afcindia.com',
-                    oppurtunity_mob: '9856272522',
-                    oppurtunity_description: 'Contrary to popular belief, Lorem Ipsum is not simply random text. It has roots in a piece of classical Latin literature from 45 BC, making it over 2000 years old. Richard McClintock, a Latin professor at Hampden-Sydney College in Virginia, looked up one of the more obscure Latin words, consectetur, from a Lorem Ipsum passage, and going through the cites of the word in classical literature, discovered the undoubtable source. Lorem Ipsum comes from sections 1.10.32 and 1.10.33 of "de Finibus Bonorum et Malorum" (The Extremes of Good and Evil) by Cicero, written in 45 BC. This book is a treatise on the theory of ethics, very popular during the Renaissance. The first line of Lorem Ipsum, "Lorem ipsum dolor sit amet..", comes from a line in section 1.10.32.'
-                },
-                {
-                    oppurtunity_id: 2,
-                    oppurtunity_code: 'AF17545',
-                    employer_name: 'AFC INDIA LIMITED',
-                    oppurtunity_designation: 'Mechnanic',
-                    oppurtunity_postDate: '29-01-2025',
-                    oppurtunity_lastDate: '02-02-2025',
-                    oppurtunity_incharge: 'Ms.Stella',
-                    oppurtunity_email: 'hr@afcindia.com',
-                    oppurtunity_mob: '9856272522',
-                    oppurtunity_description: 'job title for Testing'
-                },
-                {
-                    oppurtunity_id: 3,
-                    oppurtunity_code: 'AF17555',
-                    employer_name: 'AFC INDIA LIMITED',
-                    oppurtunity_designation: 'Chemist',
-                    oppurtunity_postDate: '29-01-2025',
-                    oppurtunity_lastDate: '02-02-2025',
-                    oppurtunity_incharge: 'Ms.Stella',
-                    oppurtunity_email: 'hr@afcindia.com',
-                    oppurtunity_mob: '9856272522',
-                    oppurtunity_description: 'job title for Testing'
-                },
-            ],
-        }
-    },
-    mounted() {
-        if (sessionStorage.getItem('loggedEmail') === 'admin@afcindia.com') {
-            this.canApprove = true;
-        }
-    },
-    methods: {
-        viewJd(d) {
-            this.visible = true;
-            this.disabledFlag = true;
-            this.jdcode = d.oppurtunity_code;
-             this.empname = d.employer_name;
-            this.postDate = d.oppurtunity_postDate;
-            this.lastDate = d.oppurtunity_lastDate;
-            this.incharge = d.oppurtunity_incharge;
-            this.designation = d.oppurtunity_designation;
-            this.emailId = d.oppurtunity_email;
-            this.mobile = d.oppurtunity_mob;
-            this.description = d.oppurtunity_description
-            this.location = d.oppurtunity_location
-        },
-        
-        doApprove() {
-            console.log("Approve Button clicked !!")
-        },
-        doSubmit() {
-            console.log("Submit Button clicked !!")
-        },
-    }
+toUI(dateStr) {
+  if (!dateStr) return "";
+  const d = new Date(dateStr);
+  return (
+    ("0" + d.getDate()).slice(-2) + "/" +
+    ("0" + (d.getMonth() + 1)).slice(-2) + "/" +
+    d.getFullYear()
+  );
 }
+},
+mounted() {
+  this.fetchAll();
+}
+};
 </script>
 <style scoped>
-.padding8 {
-    padding-top: 8px;
-}
-
-.flright {
-    float: left;
-}
-
 .item {
     font-weight: 700;
     display: flex;
-}
-.center {
-  text-align: center;
-  /* border: 1px solid green; */
 }
 </style>
